db418218668fadfd0ff8d6ace1bb90c1a5e60185
matthew-dean-brown <mattrewbrown@gmail.com>
2024-08-19 09:18:37 +0200

    hehe frontend

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> | 
+    <router-link to="/fruits">Fruits</router-link> 
+    <div v-if="!$cookies.get('token')">
+      |
+      <router-link to="/login">Login</router-link>
+    </div>
+    <div v-else>
+      <button @click="logout()">Log Out</button>
+    </div>
+  </nav>
+  <router-view/>
+</template>
+<script>
+export default{
+  methods:{
+    logout(){
+      !$cookies.remove('token')
+      location.reload()
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/views/FruitView.vue
@@ -0,0 +1,28 @@
+<template>
+    <h1>Fruits hehe</h1>
+    <div>
+        <section v-for="fruit in $store.state.fruits" :key="fruit.id">
+            {{ fruit.id  }}
+            {{ fruit.fruit_name }}
+            {{ fruit.weight }}
+            {{ fruit.amount }}
+            <button v-if="$cookies.get('token')" @click="addToCart(fruit.id)">Purchase</button>
+        </section>
+    </div>
+</template>
+
+<script>
+export default{
+    methods:{
+        getFruits(){
+        this.$store.dispatch('getFruits')
+      },
+      addToCart(fruit_id){
+        this.$store.dispatch('addToCart',fruit_id)
+      }
+    },
+    mounted(){
+        this.getFruits()
+    }
+}
+</script>
